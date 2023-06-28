52a1f976b096fcc0979a17f358ca6ff9b5c2b358
Loki99911 <nikita_kresik@i.ua>
2023-06-28 19:21:29 +0300

    add CORS

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const scoreRouter = require("./routes/api/scoreRouts");
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 // app.use(logger(formatsLogger));
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/score", scoreRouter);
