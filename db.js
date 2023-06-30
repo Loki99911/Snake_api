94b8dc155cb5b22b47ce829884d8a37713ea3dc8
Loki99911 <nikita_kresik@i.ua>
2023-06-30 21:36:29 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,35 @@
+// const Pool = require("pg").Pool;
+// const pool = new Pool({
+//   user: "snakedb_9u9o_user", // Замените на свое имя пользователя
+//   password: "kico8WAVRLqHgOQ2rAeYWPYFGLwogUPF", // Замените на свой пароль
+//   host: "snakedb_9u9o-e.Frankfurt-postgres.render.com", // Замените на свой хост
+//   port: 5432, // Порт по умолчанию для PostgreSQL
+//   database: "snakedb_9u9o", // Замените на свою базу данных
+// });
+
+// module.exports = pool;
+
+// ..................................
+// const Pool = require("pg").Pool;
+// const pool = new Pool({
+//   user: "postgres", // Замените на свое имя пользователя
+//   password: "595280", // Замените на свой пароль
+//   host: "localhost", // Замените на свой хост
+//   port: 5432, // Порт по умолчанию для PostgreSQL
+//   database: "snake_score", // Замените на свою базу данных
+// });
+
+// module.exports = pool;
+
+// ..................................
 const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "snakedb_9u9o_user", // Замените на свое имя пользователя
   password: "kico8WAVRLqHgOQ2rAeYWPYFGLwogUPF", // Замените на свой пароль
-  host: "snakedb_9u9o-e.Frankfurt-postgres.render.com", // Замените на свой хост
+  host: "localhost", // Замените на свой хост
   port: 5432, // Порт по умолчанию для PostgreSQL
   database: "snakedb_9u9o", // Замените на свою базу данных
 });
 
 module.exports = pool;
+
