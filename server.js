061ec650e86d2f99f9ff0fc30ccc5c677d055b2f
Loki99911 <nikita_kresik@i.ua>
2023-06-30 15:29:12 +0300

    del Host

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const scoreRouter = require("./routes/api/scoreRouts");
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
-const HOST = process.env.HOST_DB;
 const app = express();
 // console.log(PORT);
 // console.log(process.env.HOST_DB);
@@ -13,4 +12,4 @@ app.use(express.json());
 
 app.use("/api/score", scoreRouter);
 
-app.listen(PORT, HOST, () => console.log(`Server run on port:${PORT}`));
+app.listen(PORT, () => console.log(`Server run on port:${PORT}`));
