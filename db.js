465d42cee9c88717a844f20dddf420b46d1428b7
Loki99911 <nikita_kresik@i.ua>
2023-07-01 10:18:51 +0300

    db local

--- db.js
+++ db.js
@@ -10,26 +10,26 @@
 // module.exports = pool;
 
 // ..................................
-// const Pool = require("pg").Pool;
-// const pool = new Pool({
-//   user: "postgres", // Замените на свое имя пользователя
-//   password: "595280", // Замените на свой пароль
-//   host: "localhost", // Замените на свой хост
-//   port: 5432, // Порт по умолчанию для PostgreSQL
-//   database: "snake_score", // Замените на свою базу данных
-// });
-
-// module.exports = pool;
-
-// ..................................
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: "snakedb_9u9o_user", // Замените на свое имя пользователя
-  password: "kico8WAVRLqHgOQ2rAeYWPYFGLwogUPF", // Замените на свой пароль
+  user: "postgres", // Замените на свое имя пользователя
+  password: "595280", // Замените на свой пароль
   host: "localhost", // Замените на свой хост
   port: 5432, // Порт по умолчанию для PostgreSQL
-  database: "snakedb_9u9o", // Замените на свою базу данных
+  database: "snake_score", // Замените на свою базу данных
 });
 
 module.exports = pool;
+
+// ..................................
+// const Pool = require("pg").Pool;
+// const pool = new Pool({
+//   user: "snakedb_9u9o_user", // Замените на свое имя пользователя
+//   password: "kico8WAVRLqHgOQ2rAeYWPYFGLwogUPF", // Замените на свой пароль
+//   host: "localhost", // Замените на свой хост
+//   port: 5432, // Порт по умолчанию для PostgreSQL
+//   database: "snakedb_9u9o", // Замените на свою базу данных
+// });
+
+// module.exports = pool;
 
--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const app = express();
 // app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-
 app.use("/api/score", scoreRouter);
 
 app.listen(PORT, () => console.log(`Server run on port:${PORT}`));
