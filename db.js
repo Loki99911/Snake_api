9cddecfa009349cf04e85f8df3095718ce3a6dcb
Loki99911 <nikita_kresik@i.ua>
2023-07-01 12:02:09 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
-const Pool = require("pg").Pool;
-const pool = new Pool({
-  user: "snakedb_9u9o_user", // Замените на свое имя пользователя
-  password: "kico8WAVRLqHgOQ2rAeYWPYFGLwogUPF", // Замените на свой пароль
-  host: "snakedb_9u9o-e.Frankfurt-postgres.render.com?sslmode=require", // Замените на свой хост
-  port: 5432, // Порт по умолчанию для PostgreSQL
-  database: "snakedb_9u9o", // Замените на свою базу данных
-});
+// const Pool = require("pg").Pool;
+// const pool = new Pool({
+//   user: "snakedb_9u9o_user", // Замените на свое имя пользователя
+//   password: "kico8WAVRLqHgOQ2rAeYWPYFGLwogUPF", // Замените на свой пароль
+//   host: "snakedb_9u9o-e.Frankfurt-postgres.render.com?sslmode=require", // Замените на свой хост
+//   port: 5432, // Порт по умолчанию для PostgreSQL
+//   database: "snakedb_9u9o", // Замените на свою базу данных
+// });
 
-module.exports = pool;
+// module.exports = pool;
 
 // ..................................
 // const Pool = require("pg").Pool;
@@ -33,3 +33,14 @@ module.exports = pool;
 
 // module.exports = pool;
 
+require("dotenv").config();
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: process.env.DB_USER, // Использовать значение из переменной среды DB_USER
+  password: process.env.DB_PASSWORD, // Использовать значение из переменной среды DB_PASSWORD
+  host: process.env.DB_HOST, // Использовать значение из переменной среды DB_HOST
+  port: process.env.DB_PORT, // Использовать значение из переменной среды DB_PORT
+  database: process.env.DB_NAME, // Использовать значение из переменной среды DB_NAME
+});
+
+module.exports = pool;
