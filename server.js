b448598cd3d976a50f54071425415b762f7ac4ef
Loki99911 <nikita_kresik@i.ua>
2023-07-02 20:26:24 +0300

    env.example

--- server.js
+++ server.js
@@ -5,22 +5,8 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const app = express();
 const pool = require("./db");
-// async function getAllContacts() {
-//   try {
-//     const client = await pool.connect();
-//       const result = await client.query("SELECT * FROM score");
-//       console.log(result.rows);
-//     const contacts = result.rows;
-//     client.release();
-//     return contacts;
-//   } catch (error) {
-//     console.error("Error retrieving data:", error);
-//     throw error;
-//   }
-// }
 app.use(cors());
 app.use(express.json());
-// console.log(getAllContacts());
 app.use("/api/score", scoreRouter);
 
 app.listen(PORT, () => console.log(`Server run on port:${PORT}`));
